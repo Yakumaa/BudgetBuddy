df4e25b6bc16bb19b07165d74766e844ca4689dd
Yakumaa <maharjanshrish8@gmail.com>
2023-07-19 16:22:00 +0545

    added user validation

--- money_tracker/authentication/views.py
+++ money_tracker/authentication/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+import json
 
 
 # Create your views here.
 class RegistrationView(View):
     def get(self, request):
         return render(request, "authentication/register.html")
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data["username"]
+
+        if not str(username).isalnum():
+            return JsonResponse(
+                {
+                    "username error": "username should only contain alpha numeric characters"
+                },
+                status=400,
+            )
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, "username taken")
+            return redirect("register")
+
+        return JsonResponse({"username_valid": True})
